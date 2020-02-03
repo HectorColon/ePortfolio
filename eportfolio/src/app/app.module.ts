3a4ccc12416443a1564b8ab1e4be6e8784b01b9d
Hector Colo Morales <colon_hector19@outlook.com>
2020-02-02 20:13:23 -0400

    App-Component modification

--- eportfolio/src/app/app.module.ts
+++ eportfolio/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MainframeComponent } from './mainframe/mainframe.component';
+import { MainframeComponent } from './mainframe/main-frame.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MainframeComponent } from './mainframe/mainframe.component';
     MainframeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
